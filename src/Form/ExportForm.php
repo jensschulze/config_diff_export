8a247eccb678c0fa98b56aeb960d01140af01fdf
Jens Schulze <info@jensschulze3000.de>
2017-10-11 16:15:28 +0200

    Config Form fixes

--- src/Form/ExportForm.php
+++ src/Form/ExportForm.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\config_diff_export\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -10,15 +9,6 @@ use Drupal\Core\Form\FormStateInterface;
  * Class ExportForm.
  */
 class ExportForm extends FormBase {
-
-//  /**
-//   * {@inheritdoc}
-//   */
-//  protected function getEditableConfigNames(): array {
-//    return [
-//      'config_diff_export.export',
-//    ];
-//  }
 
   /**
    * {@inheritdoc}
@@ -30,7 +20,7 @@ class ExportForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state):array {
+  public function buildForm(array $form, FormStateInterface $form_state): array {
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Export'),
@@ -43,7 +33,7 @@ class ExportForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $form_state->setRedirect('config_diff_export.export_controller_export_download');
+    $form_state->setRedirect('config_diff_export.export_download');
   }
 
 }
